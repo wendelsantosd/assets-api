81919a075e0fab4b429c3f8fe3f3f6f29bb93165
wendelsantosd <wendelwcsantos@gmail.com>
2024-09-09 16:44:16 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { server } from './shared/config/env/server';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   await app.listen(server.port);
   console.log(`Application is running on: ${await app.getUrl()}`);
